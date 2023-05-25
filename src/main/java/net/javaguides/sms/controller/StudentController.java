b170de8271ac0954ff8166d189be396d1bd79135
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2023-05-25 16:52:12 -0600

    scripts 25/05/2023

--- src/main/java/net/javaguides/sms/controller/StudentController.java
+++ src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -3,13 +3,19 @@ package net.javaguides.sms.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.service.StudentService;
 
 @Controller
 public class StudentController {
 	
 	private StudentService studentService;
+	
+	public StudentController() {
+		
+	}
 
 	public StudentController(StudentService studentService) {
 		super();
@@ -18,8 +24,12 @@ public class StudentController {
 	
 	// handler method to handle list students and return mode and view
 	@GetMapping("/students")
-	public String listStudents(Model model) {
-		model.addAttribute("students", studentService.getAllStudents());
-		return "students";
+	//public String listStudents(@RequestBody Model model) {
+	public String listStudents(@RequestBody Student student) {
+		//model.addAttribute("students", studentService.getAllStudents());
+		//return "students";
+		System.out.println("object: " + student.getFirstName()+"="+student.getEmail()+" = "+student.getLastName());
+        //studentService.saveStudent(student);
+        return "redirect:/students";
 	}
 }
